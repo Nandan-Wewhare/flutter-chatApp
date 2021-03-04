9921747603cd8c597ef9e9acca7a0e27158d1169
Nandan-Wewhare <nandanwewhare25@gmail.com>
2021-03-04 13:25:11 +0530

    1.1 release

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,12 +10,7 @@ import 'package:get/get.dart';
 
 import 'chat_screen.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
